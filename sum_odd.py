0bef35791288091bc23a5b039cc300af7f27e4b1
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-05-11 22:51:41 +0500

    sum-odd

--- sum_odd.py
+++ sum_odd.py
@@ -4,3 +4,14 @@
 #Create a variable "sum_odd" and assign it 0.
 
 #Find the sum of the odd digits in the variable "var_int".
+
+n = 4328
+a = n%10
+b = n//10
+s = b%10
+d = n//100
+e = d%10
+f = d//10
+result = a*(a%2)+s*(s%2)+e*(e%2)+f*(f%2)
+print(result)
+
