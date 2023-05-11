0451bbe15ca598830f1d0844806bde9d4e2dde1d
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-05-11 22:51:09 +0500

    sum-even

--- sum_even.py
+++ sum_even.py
@@ -4,3 +4,14 @@
 #Create a variable "sum_even" and assign it 0.
 
 #Find the sum of the even digits in the variable "var_int".
+
+
+n = 4328
+a = n%10
+b = n//10
+s = b%10
+d = n//100
+e = d%10
+f = d//10
+result = a*((a+1)%2)+s*((s+1)%2)+e*((e+1)%2)+f*((f+1)%2)
+print(result)
